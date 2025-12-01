996f9f8d4adadac1a9bbdab763014b1f06f5fbcc
Gary Tang <94276983+Garytang412@users.noreply.github.com>
2025-12-01 09:57:39 -0600

    Update version-test-github.meta.js

--- version-test-github.meta.js
+++ version-test-github.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Version control test from github
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Version control test
 // @downloadURL  https://github.com/Garytang412/version-test/raw/refs/heads/main/version-test-github.user.js
 // @updateURL    https://github.com/Garytang412/version-test/raw/refs/heads/main/version-test-github.meta.js
