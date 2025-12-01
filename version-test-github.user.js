7f7597379148e607889a5dbe25818385563a132f
Gary Tang <94276983+Garytang412@users.noreply.github.com>
2025-12-01 09:52:37 -0600

    Create version-test-github.user.js

---
+++ version-test-github.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Version control test from github
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Version control test2 script
+// @downloadURL  
+// @author       ptangary
+// @match        https://www.google.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    const box = document.createElement('div');
+    box.style.cssText = 'position:fixed;top:10px;right:10px;background:blue;color:red;padding:10px;border:1px solid black;z-index:9999;';
+    box.textContent = 'Version2 Test v1.1';
+    document.body.appendChild(box);
+})();
