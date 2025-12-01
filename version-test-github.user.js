fb7c98b765638943a0c3f26c16c7d27c34e3d1aa
Gary Tang <94276983+Garytang412@users.noreply.github.com>
2025-12-01 09:59:06 -0600

    Update version-test-github.user.js

--- version-test-github.user.js
+++ version-test-github.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Version control test from github
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Version control test
 // @downloadURL  https://github.com/Garytang412/version-test/raw/refs/heads/main/version-test-github.user.js
 // @updateURL    https://github.com/Garytang412/version-test/raw/refs/heads/main/version-test-github.meta.js
@@ -15,6 +15,6 @@
     
     const box = document.createElement('div');
     box.style.cssText = 'position:fixed;top:10px;right:10px;background:green;color:red;padding:10px;border:1px solid black;z-index:9999;';
-    box.textContent = 'Github v 1.1';
+    box.textContent = 'Github v 1.2';
     document.body.appendChild(box);
 })();
