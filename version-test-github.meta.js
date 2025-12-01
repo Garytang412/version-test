01f61dd679f692e2e40c176fa1ca7480a0963704
Gary Tang <94276983+Garytang412@users.noreply.github.com>
2025-12-01 09:55:44 -0600

    Create version-test-github.meta.js

---
+++ version-test-github.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Version control test from github
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Version control test
+// @downloadURL  https://github.com/Garytang412/version-test/raw/refs/heads/main/version-test-github.user.js
+// @updateURL    https://github.com/Garytang412/version-test/raw/refs/heads/main/version-test-github.meta.js
+// @author       ptangary
+// @match        https://www.google.com/*
+// @grant        none
+// ==/UserScript==
